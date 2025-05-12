effd2f73851f1f3eb2a7826b1eec7fed37f06bac
chaeyoung <happy7yong83@gmail.com>
2025-05-12 20:11:02 +0900

    feat : Mesh, light, useFrame 실습

--- tstR3F/src/App.jsx
+++ tstR3F/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import {Canvas} from "@react-three/fiber";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import MyElement3D from "./Components/MyElement3D.jsx";
 
 function App() {
-
   return (
     <>
-        <Canvas>
-
-        </Canvas>
+      <Canvas>
+        <MyElement3D />
+      </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tstR3F/src/Components/MyElement3D.jsx
+++ tstR3F/src/Components/MyElement3D.jsx
@@ -1,5 +1,21 @@
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
 function MyElement3D() {
-  return <></>;
+  const refMesh = useRef();
+  useFrame((state, delta) => {
+    refMesh.current.rotation.y += delta;
+  });
+
+  return (
+    <>
+      <directionalLight position={[1, 1, 1]} />
+      <mesh ref={refMesh} rotation-y={(45 * Math.PI) / 180}>
+        <boxGeometry />
+        <meshStandardMaterial color="#e67e22" />
+      </mesh>
+    </>
+  );
 }
 
 export default MyElement3D;
